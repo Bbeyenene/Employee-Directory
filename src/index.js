3792b9279d9ac71d554beb4a79b8c81b3c09e1bf
berhane beyene <monoxica2004@yahoo.com>
2020-11-13 13:30:17 -0600

    src/index.js commit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as serviceWorker from './serviceWorker';
+import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+
 
 ReactDOM.render(
   <React.StrictMode>
@@ -15,3 +19,6 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
