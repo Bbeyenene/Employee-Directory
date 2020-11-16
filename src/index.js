2e76560fb5d170eb59ea53ba4f5cff4d664bbcc8
berhane beyene <monoxica2004@yahoo.com>
2020-11-16 14:04:36 -0600

    index.js modified

--- src/index.js
+++ src/index.js
@@ -1,9 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-<<<<<<< HEAD
-=======
-//import * as serviceWorker from './serviceWorker';
->>>>>>> 275aafb57f5d7cc10422c6e385b0c7e903db928e
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
@@ -18,10 +14,9 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
 
-// // Learn more about service workers: https://bit.ly/CRA-PWA
-// serviceWorker.unregister();
